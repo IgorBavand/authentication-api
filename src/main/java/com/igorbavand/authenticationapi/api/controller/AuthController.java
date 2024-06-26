f894e49d4d56e13adf51eff4388c083f4952e03f
Guerreiro <igor.bvn@gmail.com>
2024-06-26 08:18:52 -0300

    refactor: implements AuthService

--- src/main/java/com/igorbavand/authenticationapi/api/controller/AuthController.java
+++ src/main/java/com/igorbavand/authenticationapi/api/controller/AuthController.java
@@ -1,18 +1,16 @@
 package com.igorbavand.authenticationapi.api.controller;
 
 import com.igorbavand.authenticationapi.api.dto.AuthenticationRequest;
-import com.igorbavand.authenticationapi.api.dto.AuthenticationResponse;
 import com.igorbavand.authenticationapi.api.dto.TokenRefreshRequest;
 import com.igorbavand.authenticationapi.api.dto.TokenRefreshResponse;
 import com.igorbavand.authenticationapi.domain.RefreshToken;
 import com.igorbavand.authenticationapi.domain.User;
+import com.igorbavand.authenticationapi.domain.service.AuthService;
 import com.igorbavand.authenticationapi.domain.service.RefreshTokenService;
 import com.igorbavand.authenticationapi.domain.service.UserService;
 import com.igorbavand.authenticationapi.infra.utils.JwtUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,13 +23,20 @@ public class AuthController {
     private final JwtUtil jwtUtil;
     private final UserService userService;
     private final RefreshTokenService refreshTokenService;
+    private final AuthService authService;
 
-    public AuthController(AuthenticationManager authenticationManager, UserDetailsService userDetailsService, JwtUtil jwtUtil, UserService userService, RefreshTokenService refreshTokenService) {
+    public AuthController(AuthenticationManager authenticationManager,
+                          UserDetailsService userDetailsService,
+                          JwtUtil jwtUtil,
+                          UserService userService,
+                          RefreshTokenService refreshTokenService,
+                          AuthService authService) {
         this.authenticationManager = authenticationManager;
         this.userDetailsService = userDetailsService;
         this.jwtUtil = jwtUtil;
         this.userService = userService;
         this.refreshTokenService = refreshTokenService;
+        this.authService = authService;
     }
 
     @PostMapping("/register")
@@ -42,31 +47,12 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) {
-        authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
-        );
-
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
-        final String jwt = jwtUtil.generateToken(userDetails.getUsername());
-
-        User user = userService.findByUsername(authenticationRequest.getUsername());
-        RefreshToken refreshToken = refreshTokenService.createRefreshToken(user.getId());
-
-        return ResponseEntity.ok(new AuthenticationResponse(jwt, refreshToken.getToken()));
+        return authService.getToken(authenticationRequest);
     }
 
     @PostMapping("/refresh-token")
     public ResponseEntity<?> refreshToken(@RequestBody TokenRefreshRequest request) {
-        String requestRefreshToken = request.getRefreshToken();
-
-        return refreshTokenService.findByToken(requestRefreshToken)
-            .map(refreshTokenService::verifyExpiration)
-            .map(RefreshToken::getUser)
-            .map(user -> {
-                String token = jwtUtil.generateToken(user.getUsername());
-                return ResponseEntity.ok(new TokenRefreshResponse(token, requestRefreshToken));
-            })
-            .orElseThrow(() -> new RuntimeException("Refresh token is not in database!"));
+        return authService.refreshToken(request);
     }
 
     @PostMapping("/assign-role")
---
+++ src/main/java/com/igorbavand/authenticationapi/domain/service/AuthService.java
@@ -0,0 +1,61 @@
+package com.igorbavand.authenticationapi.domain.service;
+
+import com.igorbavand.authenticationapi.api.dto.AuthenticationRequest;
+import com.igorbavand.authenticationapi.api.dto.AuthenticationResponse;
+import com.igorbavand.authenticationapi.api.dto.TokenRefreshRequest;
+import com.igorbavand.authenticationapi.api.dto.TokenRefreshResponse;
+import com.igorbavand.authenticationapi.domain.RefreshToken;
+import com.igorbavand.authenticationapi.domain.User;
+import com.igorbavand.authenticationapi.infra.utils.JwtUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private final JwtUtil jwtUtil;
+    private final UserService userService;
+    private final RefreshTokenService refreshTokenService;
+
+    public AuthService(AuthenticationManager authenticationManager, UserDetailsService userDetailsService, JwtUtil jwtUtil, UserService userService, RefreshTokenService refreshTokenService) {
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
+        this.jwtUtil = jwtUtil;
+        this.userService = userService;
+        this.refreshTokenService = refreshTokenService;
+    }
+
+    public ResponseEntity<?> getToken(AuthenticationRequest authenticationRequest) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+        );
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails.getUsername());
+
+        User user = userService.findByUsername(authenticationRequest.getUsername());
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(user.getId());
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt, refreshToken.getToken()));
+    }
+
+    public ResponseEntity<?> refreshToken(TokenRefreshRequest request) {
+        String requestRefreshToken = request.getRefreshToken();
+
+        return refreshTokenService.findByToken(requestRefreshToken)
+            .map(refreshTokenService::verifyExpiration)
+            .map(RefreshToken::getUser)
+            .map(user -> {
+                String token = jwtUtil.generateToken(user.getUsername());
+                return ResponseEntity.ok(new TokenRefreshResponse(token, requestRefreshToken));
+            })
+            .orElseThrow(() -> new RuntimeException("Refresh token is not in database!"));
+    }
+
+}
